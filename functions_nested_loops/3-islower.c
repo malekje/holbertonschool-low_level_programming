e38602f8ae0d3f2fc8343c9849ea366b9e14cddf
malekje <loukatankionline@gmail.com>
2022-06-09 11:24:27 -0700

    lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _islower - Entry point
+ * Return:  0 if uppercase and 1 if lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	}
