73632e8d44a7372288ef8dd5990af656fd073624
malekje <loukatankionline@gmail.com>
2022-06-09 11:08:50 -0700

    print alpha 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+	{
+		char alphabet;
+		int i;
+
+		for (i = 1; i<= 10; i++)
+		{
+			for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			{
+				putchar(alphabet);
+			}
+			putchar('\n');
+		}
+	}
