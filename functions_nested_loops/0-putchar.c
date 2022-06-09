14c264050e943c73404400c6919baa82a4c80bea
malekje <loukatankionline@gmail.com>
2022-06-09 04:36:24 -0700

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+	char ch[] = "_putchar";
+
+	for (s = 0; s <= 7; s++)
+		_putchar(ch[s]);
+	_putchar('\n');
+	return (0);
+}
