a5b26908971892f5541b5024082b247a4f2a665b
malekje <loukatankionline@gmail.com>
2022-06-09 00:14:43 -0700

    last dig

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
-					printf("last digit of %d is %d and is", n, (n % 10));
+					printf("last digit of %d is %d and is ", n, (n % 10));
 					if ((n % 10) == 0)
 					{
 						printf("0\n");
