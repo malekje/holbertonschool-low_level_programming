cff553f0c72d050cedf40ad48b71279691eba78f
malekje <loukatankionline@gmail.com>
2022-06-09 11:33:45 -0700

    only alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _isalpha - Entry point
+ * Return: 1 if upper or lower and 0 if not alphabet
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		{
+			return (0);
+		}
+}
