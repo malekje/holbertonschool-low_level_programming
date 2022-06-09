600bbfb2a06b42b7d553638492bacbbd8168f66a
malekje <loukatankionline@gmail.com>
2022-06-09 12:53:55 -0700

    fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,23 +2,23 @@
 /**
  * print_sign - main entry
  * @n: checking
- * Return: 0
+ * Return: 1
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		_putchar(43);
 	 	return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(48);
+		_putchar(48);
 		return (0);
 	}
 	else
 	{
-		putchar(45);
+		_putchar(45);
 	        return (-1);
 	}
 }
