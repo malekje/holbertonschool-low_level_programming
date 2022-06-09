d9e5de366ef9523241253fa647d054f636c0ded6
malekje <loukatankionline@gmail.com>
2022-06-09 01:04:48 -0700

    print lower than upper

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return : always 0
+ */
+int main(void)
+{
+	char l = 'a';
+	char u = 'A';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+
+	while (u <= 'Z')
+	{
+	putchar(u);
+u++;
+	}
