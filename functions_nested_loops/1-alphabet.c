50f6536a7aba8d268f081ebacd490a2f8b9a7d0f
malekje <loukatankionline@gmail.com>
2022-06-09 09:36:36 -0700

    alpha

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_alphabet - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+	
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n'):
+}
