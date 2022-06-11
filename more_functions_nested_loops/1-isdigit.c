1fba60722c5c6e880f97a42ed33e0220202a9f11
malekje <loukatankionline@gmail.com>
2022-06-11 04:05:48 -0700

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - main entry
+ * @c: checking
+ * Return: 1 if c a digit else 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
