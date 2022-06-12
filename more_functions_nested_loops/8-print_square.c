bc70ccda1ecf2fffe0c10fde3afa7f91a2aad7e8
malekje <loukatankionline@gmail.com>
2022-06-12 02:34:55 -0700

    size q

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * print_square - main entry
+ * @size: checking
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int x = 0, y = 0;
+
+	if (size > 0)
+	{
+		for (x = 1; y <= x; x++)
+		{
+			for (y = 1; x <= size; y++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+		putchar('\n');
+}
