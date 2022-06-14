ba4c328d1c06202a95073974afd7147b66d90b6b
malekje <loukatankionline@gmail.com>
2022-06-14 02:19:57 -0700

    prototypes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,7 +11,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int putchar(int char);
+int _putchar(char c);
 
 
 #endif
