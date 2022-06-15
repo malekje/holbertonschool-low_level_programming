c62764f4aae2be172f3f1658d634c22749b85c49
malekje <loukatankionline@gmail.com>
2022-06-15 00:43:48 -0700

    first char

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * puts2 - main entry 
+ * puts2 - main entry
  * @str: string
  */
 void puts2(char *str)
 {
 	int n = 0;
 
-	while (*(str + n) != '\0');
+	while (*str + n != '\0');
 	++n;
 	{
 		if (n % 2 == 0)
