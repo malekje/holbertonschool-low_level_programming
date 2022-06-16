98397f7a370058f589a2e2794b2d6ae5f0ed7875
malekje <loukatankionline@gmail.com>
2022-06-16 07:28:57 -0700

    lower to upper string

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *string_toupper - main entry
+ * @s: string
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+
+int n;
+
+for (n = 0; s[n] != '\0'; n++)
+{
+
+	if (s[n] <= 'z' && s[n] >= 'a')
+
+		s[n] = s[n] - 'A' + 'a';
+}
+return (s);
+}
