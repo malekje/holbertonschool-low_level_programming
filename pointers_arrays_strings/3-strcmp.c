167045e4136269dc892d6864fb600e3a0d8e4052
malekje <loukatankionline@gmail.com>
2022-06-16 07:16:16 -0700

    comparing

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - main entry
+ * @s1: string
+ * @s2: string
+ * Return: number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		*s1++;
+		*s2++;
+	}
+}
