79a14ba7451980959079609df1db16ebac3c2fc1
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-21 11:27:06 +0100

    print string

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,11 +2,20 @@
 /**
  * _puts_recursion - main entry
  * @s: string
+ * Return: string
  */
 
 void _puts_recursion(char *s)
 {
-    puts(%s);
-    _putchar(\n);
-    _puts_recursion(char *s)
+    if (*s == '\0')
+    {
+        _putchar('\n');
+    
+    return;
+    }
+
+
+    _putchar(*s);
+    s++;
+    _puts_recursion(s);
 }
