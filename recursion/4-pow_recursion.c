4b528399bff4d1b0266dd1631e05a9b4274553e5
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-22 08:00:34 +0100

    power x y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - main entry
+ * @x: integer
+ * @y: integer
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+if (x == 0)
+return (0);
+if (x < 0)
+return (-1);
+return(_pow_recursion(x, y - 1 * x));
+}
