4daf4f6761c0cc27524e3d703ba43ba53ad0611f
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-22 08:02:31 +0100

    power x y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -7,9 +7,9 @@
  */
 int _pow_recursion(int x, int y)
 {
-if (x == 0)
-return (0);
-if (x < 0)
-return (-1);
-return(_pow_recursion(x, y - 1 * x));
+    if (x == 0)
+    return (0);
+    if (x < 0)
+    return (-1);
+    return (_pow_recursion(x, y - 1) * x);
 }
