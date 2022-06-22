691ea106c42b41b1779d01b039b9a0cf5e7ae929
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-22 08:28:35 +0100

    sqrt

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * sqrt - main entry
+ * @x: integer
+ * @y: integer
+ * Return: Always 0.
+ */
+int _sqrt(int x, int y)
+{
+if (y * y == x)
+{
+return (y);
+}
+if (y * y > x)
+{
+return (-1);
+}
+return (_sqrt(x, y + 1));
+}
+
+
+/**
+* _sqrt_recursion - main entry
+* @n: integer
+* Return: natural square root
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return(-1);
+}
+return(_sqrt(n, 0));
+}
