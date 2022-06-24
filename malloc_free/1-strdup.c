6fe60ba0e6bc7957c6f7144156e0841a98d4e590
malekje <loukatankionline@gmail.com>
2022-06-24 09:24:30 -0700

    return pointer

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,29 +1,25 @@
 #include "main.h"
 /**
- *  * *_strdup - main entry
- *   * @str: string
- *    * Return: 0
- *     */
+ * *_strdup - main entry
+ * @str: string
+ * Return: 0
+ */
 char *_strdup(char *str)
 {
-	char *h; 
-	char *b;
+
+	char *a, *h;
 	int size = strlen(str);
 
-	if (h == NULL)
-	{
-		return (NULL);
-	}
-	h = b;
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-	h = malloc(sizeof(char) * (size + 1));
-	while (*str)
-	{
-		*b++ = *str++;
+	a = malloc(sizeof(char) * (size + 1));
+	if (a == NULL)
+		return (NULL);
 
-	return (h);
-	}
+	h = a;
+
+	while (*str)
+		*h++ = *str++;
+
+	return (a);
 }
