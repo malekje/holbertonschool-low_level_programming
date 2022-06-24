574f96bd3874361b8519601b9ac34ec88456629b
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-24 09:46:51 +0100

    print its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - main entry
+* @argc: argument counter
+* @argv: argument value
+* Return: always (0)
+*/
+int main(int argc, char *argv[])
+{
+if (argc >= 1)
+printf("%s\n", argv[1]);
+return (0);
+}
