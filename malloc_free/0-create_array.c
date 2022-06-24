fa3156d3150e2635c2e8413c5a0448afe438d936
malekje <loukatankionline@gmail.com>
2022-06-24 08:26:38 -0700

    arr of char and init with special char

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - main entry
+ * @size: integer
+ * @c: character
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int n;
+
+	if (size <= 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (n = 0; n < size; n++)
+		arr[n] = c;
+			return (arr);
+}
