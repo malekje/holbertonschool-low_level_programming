a20fbf835f3160613c73259536d19e72cd35ddc0
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-24 10:32:51 +0100

    print n of arg

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,12 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 /**
 * main - main entry
 * @argc: argument counter
 * @argv: argument value
-* Return: 0
+* Return: (0)
 */
 int main(int argc, char **argv)
 {
+    (void)argv;
 if (argc > 0)
 printf("%d", argc -1);
 putchar('\n');
