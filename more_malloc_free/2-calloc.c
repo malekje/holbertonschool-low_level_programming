6ed734c8fc4c2c88ddbd4e24dc1f7b4b812d763c
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-29 11:45:42 +0100

    alocate memory using array

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* *_calloc - main entry
+* @nmemb: size
+* @size: size
+* Return: array
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	void *ptr;
+	char *result;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+	return (NULL);
+	result = ptr;
+	for (i = 0; i < ptr; i++)
+	{
+	result[i] = '\0';
+	}
+	return (ptr);
+}
