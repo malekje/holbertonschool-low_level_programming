9f68aeb327773c78e282e72e669886badfdf819e
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-29 10:05:35 +0100

    allocate using malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* *malloc_checked - main entry
+* @b: size
+* Return: ..
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	exit(98);
+	return (p);
+}
