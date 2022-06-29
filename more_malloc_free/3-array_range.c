172a0f82164352a7f2904d0541449b9c3d30aca4
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-29 11:59:50 +0100

    create arr of int

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* *array_range - main entry
+* @min: integer
+* @max: integer
+* Return: array of integers
+*/
+int *array_range(int min, int max)
+{
+	int r, i, *array;
+
+	r = (max - min + 1);
+
+	if (min > max)
+	return (NULL);
+	array = malloc(sizeof(int) * r);
+	if (array == NULL)
+	return (NULL);
+	for (i = 0; i < r; i++)
+	array[i] = min = min + 1;
+	return (array);
+}
