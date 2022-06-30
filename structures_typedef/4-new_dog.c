2ec1464c919fafbd120885aa02feb8a42437d7c8
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-30 13:58:54 +0100

    create new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,31 +9,34 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i = 1;
-	int n = 0;
+	
 	dog_t *dog;
 
 
-	if (name || owner == NULL || age < n)
+	if ((name && owner == NULL) || (age < 0))
 	return (NULL);
 
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
+	{
 	return (NULL);
-	dog->name = malloc(strlen(name) + i);
+	}
+	dog->name = malloc(strlen(name) + 1);
 
 	if (dog->name == NULL)
+	{
 	free(dog);
 	return (NULL);
-
-	dog->owner = malloc(strlen(owner) + i);
+	}
+	dog->owner = malloc(strlen(owner) + 1);
 	if (dog->owner == NULL)
+	{
 	free(dog->owner);
 	free(dog->name);
 	free(dog);
 	return (NULL);
-
+	}
 	dog->name = strcpy(dog->name, name);
 	dog->age = age;
 	dog->owner = strcpy(dog->owner, owner); 
