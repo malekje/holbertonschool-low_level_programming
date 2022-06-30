58279082761e23fe338399d54888e92397da0a45
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-30 14:05:45 +0100

    need fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,31 +10,5 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	
-	dog_t *dog;
 	
-	dog = malloc(sizeof(dog_t));
-
-	if (dog == NULL)
-	{
-	return (NULL);
-	}
-	dog->name = malloc(strlen(name) + 1);
-
-	if (dog->name == NULL)
-	{
-	free(dog);
-	return (NULL);
-	}
-	dog->owner = malloc(strlen(owner) + 1);
-	if (dog->owner == NULL)
-	{
-	free(dog->owner);
-	free(dog->name);
-	free(dog);
-	return (NULL);
-	}
-	dog->name = strcpy(dog->name, name);
-	dog->age = age;
-	dog->owner = strcpy(dog->owner, owner); 
-	return(dog);
 }
