5783d058e003526d88de709cc4a70959e6f06a15
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-30 11:25:55 +0100

    prototypes

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define _dog_h
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+
 
 /**
  * struct dog - information for a dog
