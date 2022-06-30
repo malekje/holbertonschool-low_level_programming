e1de7d485077d697a92d6b42a10121adab8c34ce
root <root@DESKTOP-A2UTKT3.localdomain>
2022-06-30 12:31:18 +0100

    print struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - main entry
+* @d: dog type
+* Return: void
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	return;
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+		putchar('\n');
+	}
+	else
+	{
+		printf("Name: %s", d->name);
+		putchar('\n');
+	}
+	if (d->age < 1)
+	{
+		printf("age: (nil)");
+		putchar('\n');
+	}
+	else
+	{
+		printf("age: %f", d->age);
+		putchar('\n');
+	}
+	if (d->owner == NULL)
+	{
+		printf("owner= (nil)");
+		putchar('\n');
+	}
+	else
+	{
+		printf("owner: %s", d->owner);
+		putchar('\n');
+	}
+}
