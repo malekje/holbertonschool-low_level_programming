2a8d54803ccf24a2004d9f64e8c65bc7e545af94
malekje <loukatankionline@gmail.com>
2022-07-01 11:22:11 -0700

    sreach of an integer

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - main entry
+ * @array: array
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: nothing.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	else if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) + i)
+			return(i);
+	return(-1);
+
+}
+
