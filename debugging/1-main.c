b65741b0bd9c219a3fbe937adaa448baf997db15
malekje <loukatankionline@gmail.com>
2022-07-03 14:43:08 -0700

    fixing

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * * main - causes an infinite loop
- * * Return: 0
- * */
+ * main - causes an infinite loop
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -18,9 +18,7 @@ int main(void)
 	*    		putchar(i);
 	*          }
 	*/
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
-
