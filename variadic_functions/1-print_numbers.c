0c43b161fe5ff647977ce797bc27ba343f265f70
root <root@DESKTOP-A2UTKT3.localdomain>
2022-07-04 11:58:41 +0100

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -21,6 +21,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (i != n && separator != NULL)
 	printf("%s", separator);
 	}
+	putchar('\n');
 	va_end(numbers);
-	printf("\n");
 }
