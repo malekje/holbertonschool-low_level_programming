831aed6a0e6d8d3602c3a4c4d25155fb46ad117a
root <root@DESKTOP-A2UTKT3.localdomain>
2022-07-04 12:45:30 +0100

    print anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+
+void c(va_list character)
+{
+	printf("%c", va_arg(character, int));
+}
+
+void i(va_list integer)
+{
+	printf("%d", va_arg(integer, int));
+}
+
+void f(va_list _float)
+{
+	printf("%f", va_arg(_float, int));
+}
+
+void s(va_list, string)
+{
+	char *str;
+	if (str == NULL)
+	{
+	printf("(nil)");
+	return;
+	}
+	printf("%s", va_arg(char *))
+}
+
+
+
+
+/**
+* print_all - main entry
+* @format: array 
+*/
+void print_all(const char * const format, ...)
+{
+
+}
