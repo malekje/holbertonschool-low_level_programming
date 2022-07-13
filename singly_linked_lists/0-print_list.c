7e8d40a35a7f5141e027ead44736e8a09395d944
Malek Mayeh <loukatankionline@gmail.com>
2022-07-13 12:55:34 +0100

    header file & prototypes

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,12 +10,13 @@ size_t print_list(const list_t *h)
 {
 	size_t n;
 	n = 0;
+	
 	while (h != NULL)
 	{
 	if (h->str == NULL)
 	printf("[0] (nil)");
 	else
-	printf("[%d] %s", h->len, h->str);
+	printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
 	n++;
 	}
