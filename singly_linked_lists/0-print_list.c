208dc8a5c302f00f06da929f91cc79551efdc467
Malek Mayeh <loukatankionline@gmail.com>
2022-07-13 13:01:30 +0100

    print all elements

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,11 +14,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 	if (h->str == NULL)
-	printf("[0] (nil)");
-	putchar('\n');
+	printf("[0] (nil)\n");
+	
 	else
-	printf("[%d] %s", h->len, h->str);
-	putchar('\n');
+	printf("[%d] %s\n", h->len, h->str);
+	
 	h = h->next;
 	n++;
 	}
