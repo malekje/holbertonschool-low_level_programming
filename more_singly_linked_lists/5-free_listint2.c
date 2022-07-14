ab0f43076351712b4ad4c76bbb624c8abe3a1373
Malek Mayeh <loukatankionline@gmail.com>
2022-07-14 16:42:03 +0100

    frees a list2

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: the linked list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *n;
+
+	if (head == NULL)
+	return;
+
+	while (*head != NULL)
+	{
+		n = *head;
+		*head = n->next;
+
+	}
+	free(n);
+}
