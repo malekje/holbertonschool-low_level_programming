b3ad16c24348802a887ffa20239678a9331addf8
Malek Mayeh <loukatankionline@gmail.com>
2022-07-14 12:23:45 +0100

    print before func executed

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
 #include "lists.h"
-/*  _main - Apply the constructor attribute so that it
+/** 
+ * _main - Apply the constructor attribute so that it
  *  is executed before main()
  *  Return: 0
  */
-void __attribute__ ((constructor)) _main() 
+void __attribute__ ((constructor)) _main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
