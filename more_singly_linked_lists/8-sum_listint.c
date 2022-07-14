4e9a14eca9fba8773a8a901e9952c3284d91a14e
Malek Mayeh <loukatankionline@gmail.com>
2022-07-14 17:29:14 +0100

    sum all the data

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - function that returns the sum of all the data (n) of a linked list
- * @head: the linked list
+ * sum_listint - function that returns the sum of all data(n) 
  * Return: sum of all data(n)
  */
 int sum_listint(listint_t *head)
