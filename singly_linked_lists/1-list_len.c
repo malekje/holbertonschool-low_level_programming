5e170f6e35569f137560c65bcc806f3cd2f0e146
Malek Mayeh <loukatankionline@gmail.com>
2022-07-14 10:37:50 +0100

    return n of elements in LL

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements in a linked list 
+ * list_len - function that returns the number of elements in a linked list
  * @h: the linked list
  * Return: number of elements
  */
