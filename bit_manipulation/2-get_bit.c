fddd8ca99d18dbaeae32816059a7af3fcd6e32a9
Malek Mayeh <loukatankionline@gmail.com>
2022-07-19 19:42:47 +0100

    return value of a bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number
+ * @index: index
+ * Return: value of a bit at a given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int a;
+	if (index >= 32) /* 32 = size of unsigned long int multiplied by 8 */
+		{
+			return (-1);
+		}
+	a = ((n >> index) & 1);
+	return (a);
+}
