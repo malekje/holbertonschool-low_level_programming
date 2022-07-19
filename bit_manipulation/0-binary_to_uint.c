4780391c2d1d543a1f788a31fd20a927e8cc1d0e
Malek Mayeh <loukatankionline@gmail.com>
2022-07-19 18:45:49 +0100

    convert bin to uns int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int 
+ * @b: binary number
+ * Return: Converted number or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum, i;
+	int n, length = strlen(b);
+	
+
+	sum = 0;
+	i = 1;
+	n = 0;
+	(void) n;
+	if (b == NULL)
+	{
+		return (sum);
+	}
+	for (length = length - 1; length >= 0; length--)
+	{
+		if (b[length] != '0' && b[length] != '1')
+		return (0);
+
+		if (b[length] == '1')
+		sum = sum + (b[length] - '0') * i;
+		i = i * 2;
+	}
+	return (sum);
+}
