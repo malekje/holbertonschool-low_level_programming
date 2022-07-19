11b7d3b3183b0ee1d666c11f5a09ca0660536c01
Malek Mayeh <loukatankionline@gmail.com>
2022-07-19 20:34:29 +0100

    clear

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,7 +9,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int clr = 1;
-	unsigned int tilde = 1;	
+	unsigned int tilde = 1;
 
 	if (index >= 32)/* 32 = size of unsigned long int multiplied by 8 */
 		return (-1);
