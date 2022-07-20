4998668594dd7ce8278fe96f12c75f21f1808370
Malek Mayeh <loukatankionline@gmail.com>
2022-07-20 13:28:40 +0100

    prototypes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 
 
 #endif
