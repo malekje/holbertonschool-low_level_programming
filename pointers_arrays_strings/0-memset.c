ea0a8b5438d4eb4d191b2fffdf7ce4c1eb6b9f74
Malek Mayeh <loukatankionline@gmail.com>
2022-07-20 13:30:55 +0100

    fills memory

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -5,20 +5,21 @@
  * @s: number
  * @b: value
  * @n: number
+ * Return: memory
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    int size = n; 
+    int size = n;
 
 	if (size >= 1)
 	{
-	
+
     int count;
 
 	for (count = 0; count < size; count++)
 	s[count] = b;
-    
+
 	}
 
 	return (s);
