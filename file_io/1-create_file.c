2401f7f5e6a62b7dcf355d3bd16b4e58653e4394
Malek Mayeh <loukatankionline@gmail.com>
2022-07-21 17:45:42 +0100

    creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file
+ * @text_content: the text
+ * Return: 1 or 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int length, writeF;
+	int openF = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	length = 0;
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		length = strlen(text_content);
+
+	writeF = write(openF, text_content, length);
+	if (writeF == -1 || openF == -1)
+		return (-1);
+	close(openF);
+	return (-1);
+}
