dfaab25a0335bb4be0e3957c6d46159ab16c453f
Malek Mayeh <loukatankionline@gmail.com>
2022-07-21 18:26:30 +0100

    add node at the start

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	node->prev = NULL;
 	node->next = *head;
 	if (*head != NULL)
-	(*head)->prev = nnode;
+	(*head)->prev = node;
 	*head = node;
 	return (node);
 }
