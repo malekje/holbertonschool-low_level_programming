bdff658da1ca84028002309ef0c96bbb8b1458a2
Malek Mayeh <loukatankionline@gmail.com>
2022-08-10 14:45:03 +0100

    print nums base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimal base 16 in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = '0';
+	int a2f = 'a';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i = i + 1;
+	}
+	while (a2f <= 'f') 
+	{
+		putchar(a2f);
+		a2f = a2f + 1;
+	}
+		putchar('\n');
+	return (0);
+}
