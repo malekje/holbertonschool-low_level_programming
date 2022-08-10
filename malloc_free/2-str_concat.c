987c488f159d2fb89a238069af64444bf98558a0
Malek Mayeh <loukatankionline@gmail.com>
2022-08-10 12:12:14 +0100

    str concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,8 +7,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i;
-	int str;
+	unsigned int i, j;
 	char *concats;
 
 	if (s1 == NULL)
@@ -28,17 +27,18 @@ char *str_concat(char *s1, char *s2)
 	if (concats == NULL)
 	return (NULL);
 
-
+	i = 0;
 	while (i < strlen(s1))
 	{
 		concats[i] = s1[i];
 		i++;
 	}
-	str = strlen(s1 + i);
-	while (i < strlen(s2))
+	j = 0;
+	while (j < strlen(s2))
 	{
-		concats[str] = s2[i];
+		concats[i] = s2[j];
 		i++;
+		j++;
 	}
 	return (concats);
 
