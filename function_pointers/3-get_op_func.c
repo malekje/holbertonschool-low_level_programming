a7dfa2c7c1f31285bc3ee2290c9e585998205a55
Malek Mayeh <loukatankionline@gmail.com>
2022-08-10 16:37:01 +0100

    get operators

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func- operators function
+ * @s: argument
+ * Return: integer
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+
+	    op_t ops[] = {
+		    {"+", op_add},
+		    {"-", op_sub},
+		    {"*", op_mul},
+		    {"/", op_div},
+		    {"%", op_mod},
+		    {NULL, NULL}
+
+	};
+
+
+	int n = 0;
+
+
+	while (strcmp(s, ops[n].op) != 0)
+		n++;
+
+    return (ops[n].f);
+
+}
