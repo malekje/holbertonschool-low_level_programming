8c7c740343a3890de6aa198f2c59d46a6cad7a21
Malek Mayeh <loukatankionline@gmail.com>
2022-08-10 20:08:06 +0100

    largest prime fac of 6....

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - print the largest prime factor of 612852475143
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long n, div = 2;
+    n = 612852475143;
+	
+
+	while (div < n)
+	{
+		if (n % div == 0)
+		{
+			n = n / div;
+			div = 2;
+		}
+
+		else
+			div++;
+	}
+	    printf("%lu\n", n);
+	return (0);
+}
