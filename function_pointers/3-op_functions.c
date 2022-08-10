c060b9a3b6788735fb0931cd0d53f4f1d9a05cca
Malek Mayeh <loukatankionline@gmail.com>
2022-08-10 16:37:12 +0100

    operators

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add- a + b
+ * @a: int
+ * @b: int
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub- a - b
+ * @a: int
+ * @b: int
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- a * b
+ * @a: int
+ * @b: int
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div- a / b
+ * @a: int
+ * @b: int
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- a % b
+ * @a: int
+ * @b: int
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
