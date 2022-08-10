3f4efceadddfe85f47b041a45457c371df8e624e
Malek Mayeh <loukatankionline@gmail.com>
2022-08-10 20:34:50 +0100

    check prime num

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * is_prime - function that checks the prime number 
+ * @i: integer
+ * @j: integer
+ * Return: 1 
+ */
+
+int isprime(int i, int div)
+{
+	if (i == div)
+		
+        return (1);
+
+	if (i % div == 0)
+		
+        return (0);
+
+	return (isprime(i, div + 1));
+
+}
+
+/**
+ * is_prime_number - function that checks the prime number 
+ * @n: integer
+ * Return: 1 
+ */
+
+int is_prime_number(int i)
+{
+    int div = 3;
+
+	if (i % 2 == 0 || i < 2)
+
+		return (0);
+
+	if (i == 2)
+
+		return (1);
+
+	return (isprime(i, div));
+
+}
