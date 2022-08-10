e3a9c07acc8d120a7a6fe588badca46553de4bdd
Malek Mayeh <loukatankionline@gmail.com>
2022-08-10 14:09:45 +0100

    prototypes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <unistd.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -18,4 +19,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 #endif
