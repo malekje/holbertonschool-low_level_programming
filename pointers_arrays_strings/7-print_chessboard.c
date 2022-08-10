e47c12de2257f124e1e211174e319841ea1d1b79
Malek Mayeh <loukatankionline@gmail.com>
2022-08-10 14:11:58 +0100

    prints the chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -5,14 +5,15 @@
  * @a: array
  * Return; nothing
  */
+
 void print_chessboard(char (*a)[8])
 {
 	int i, n;
 
-	for (i = 0; i <= 7; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (n = 0; n <= 7; n++)
+		for (n = 0; n < 8 n++)
 		_putchar(a[i][n]);
-		printf("\n");
+		_putchar('\n');
 	}
 }
