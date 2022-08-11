faae70e3017d4524f31830185ad922b44ffd54c4
Malek Mayeh <loukatankionline@gmail.com>
2022-08-11 12:18:38 +0100

    copy from file1 to file2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,10 +17,10 @@ dprintf(STDERR_FILENO, "Usage: cp file1 file2\n"), exit(97);
 file1 = open(argv[1], O_RDONLY);
 if (file1 == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file1 %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 
+file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (file2 == -1)
 {
@@ -32,7 +32,7 @@ while (n == 1024)
 n = read(file1, buffer, 1024);
 if (n == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file1 %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 i = write(file2, buffer, n);
